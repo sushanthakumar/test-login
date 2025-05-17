e99b1419f88a45546ec7edf731619d15f39cd383
sushanthakumar <sushanth.github@gmail.com>
2025-05-17 05:50:54 +0000

    updated url

--- src/utils/msalConfig.js
+++ src/utils/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
   auth: {
     clientId: "8384b874-43b1-489d-9947-28d1fd958233",
     authority: "https://saasssoapp.b2clogin.com/saasssoapp.onmicrosoft.com/B2C_1_signup_signin",
-    redirectUri: "https://happy-glacier-014cc2c00.6.azurestaticapps.net/authorized", // must match Azure B2C
+    redirectUri: "https://purple-desert-07e5a9e00.6.azurestaticapps.net/authorized", // must match Azure B2C
   },
   cache: {
     cacheLocation: "localStorage", // or sessionStorage
